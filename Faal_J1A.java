e22d8e0d72731a690f42b40d9eac4245aea08837
Mofaal <137732122+Mofaal@users.noreply.github.com>
2023-07-30 09:59:27 -0500

    Add files via upload

---
+++ Faal_J1A.java
@@ -0,0 +1,20 @@
+// My Favs - java_1A
+// Mohammad Faal - Faal23
+public class Faal_J1A{
+    public static void main(String[] args)
+    {
+        String movie, car;
+        double carCost;
+        int gradYear;
+
+        movie = "Rush Hour 2";
+        car = "Ram TRX";
+        carCost = 100000.00;
+        gradYear =2023;
+
+        System.out.println("My Favorite movie is "+ movie);
+        System.out.println("My Favorite Car is "+ car+ " and it cost " + carCost);
+        System.out.println("My Grad Year is "+ movie);
+    }
+        }
+
